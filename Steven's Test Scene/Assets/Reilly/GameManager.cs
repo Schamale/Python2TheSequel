87d24b34d23100daa6b3145da045b7d869e2f7f3
Jimmy Reilly <JimmyReilly@users.noreply.github.com>
2018-06-30 23:11:42 -0400

    Created Game Manager
    Created a Game Manager that spawns everything on start up.

---
+++ Steven's Test Scene/Assets/Reilly/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	private Swap swap;
+
+	public GameObject canvas;
+
+	public Camera camera;
+
+	public Light light;
+
+	public GameObject even;
+
+	public Button testButton;
+
+	public Button stevenButton;
+
+	void Start()
+	{
+		Instantiate (canvas);
+		Instantiate (camera);
+		Instantiate (light);
+		Instantiate (even);
+
+		GameObject controller = GameObject.FindGameObjectWithTag ("Controller");
+		swap = controller.GetComponent<Swap> ();
+
+		GameObject krisButton = GameObject.FindGameObjectWithTag ("Kris Button");
+		Button kButton = krisButton.GetComponent<Button> ();
+		kButton.onClick.AddListener(delegate {swap.ChangeScene("Kris_Test");});
+
+		GameObject mainButton = GameObject.FindGameObjectWithTag ("Steven Button");
+		Button sButton = mainButton.GetComponent<Button> ();
+		sButton.onClick.AddListener(delegate {swap.ChangeScene("Main");});
+	}
+}
