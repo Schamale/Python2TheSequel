6232585b01308a13a612006cc5ae0d80a6ee6d9e
Jimmy Reilly <JimmyReilly@users.noreply.github.com>
2018-06-30 22:52:10 -0400

    Changed Scenes
    You can now change between all of the scenes that exist as of now.

---
+++ Steven's Test Scene/Assets/Reilly/Swap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Swap : MonoBehaviour 
+{
+
+	public void ChangeScene(string name)
+	{
+		SceneManager.LoadScene (name, LoadSceneMode.Single);
+	}
+}
