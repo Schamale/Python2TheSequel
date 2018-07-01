6dcd0af87d2f0c2a371d559a0c26360cb0989168
Kris2021 <kriskriskristo@yahoo.com>
2018-07-01 16:31:30 -0400

    Camera follows player

---
+++ Steven's Test Scene/Assets/Kris_Test/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Player player;
+	public bool isFollowing = true;
+
+	public float xOffset;
+	public float yOffset;
+
+	void Start(){
+		player = FindObjectOfType<Player> ();
+
+		isFollowing = true;
+
+	}
+
+
+	void Update(){
+
+		if (isFollowing)
+		{
+			transform.position = new Vector3 (player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z); 
+		}
+	}
+}
