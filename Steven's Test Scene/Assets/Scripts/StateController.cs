19879212dca39c3c95d9b89d634dad23302cd09b
Schamale <38950754+Schamale@users.noreply.github.com>
2018-07-02 19:08:37 -0400

    Working on AI added Scripts and ScriptableObject Folder

---
+++ Steven's Test Scene/Assets/Scripts/StateController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour {
+
+    public EnemyStats enemyStats;
+    public Transform eyes;
+
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+    [HideInInspector] public BoatShooting boatShooting;
+    [HideInInspector] public List<Transform> wayPointList;
+
+    public bool aiActive;
+
+	void Awake ()
+    {
+        boatShooting = GetComponent<BoatShooting>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+    public void SetupAI(bool aiActivationFromManager, List<Transform> wayPointsFromManager)
+    {
+        wayPointList = wayPointsFromManager;
+        aiActive = aiActivationFromManager;
+        if (aiActive)
+        {
+            navMeshAgent.enabled = true;
+        }
+        else
+        {
+            navMeshAgent.enabled = false;
+        }
+    }
+}
