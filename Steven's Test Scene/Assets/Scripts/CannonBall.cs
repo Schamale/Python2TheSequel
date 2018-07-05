4f9f007fdeb09c599542a432c646d7d32df80bc9
kris2021 <40707442+kris2021@users.noreply.github.com>
2018-07-04 22:36:38 -0400

    CannonBall script added
    cannonball now destroys itself

---
+++ Steven's Test Scene/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+	private float maxLifeTime = .7f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, maxLifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
