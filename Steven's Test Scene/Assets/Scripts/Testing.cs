fff82aaa320b938017183b0bae09cfa4193311bc
kris2021 <40707442+kris2021@users.noreply.github.com>
2018-07-06 20:31:55 -0400

    added testing script
    testing script for movement

---
+++ Steven's Test Scene/Assets/Scripts/Testing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+	public Transform player;
+	public Transform wayPoint1;
+	private float maxRange = 15f;
+
+	private float moveSpeed = .05f;
+	//private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var heading = player.position - transform.position;
+
+		float step = moveSpeed * Time.deltaTime;
+		//transform.position = Vector3.MoveTowards (transform.position, wayPoint1.position, moveSpeed);
+		if (heading.sqrMagnitude < maxRange * maxRange)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, player.position, moveSpeed);
+		}
+	}
+}
