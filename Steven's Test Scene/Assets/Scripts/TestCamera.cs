742dde552cdf265cd79c8dc4fc1510ae9251a994
kris2021 <40707442+kris2021@users.noreply.github.com>
2018-07-08 13:43:48 -0400

    edited testing script

---
+++ Steven's Test Scene/Assets/Scripts/TestCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCamera : MonoBehaviour {
+
+	public GameObject player;
+	public bool isFollowing = true;
+
+	public float xOffset;
+	public float yOffset;
+
+	void Start(){
+		//player = FindObjectOfType<TestPlayer> ();
+
+		isFollowing = true;
+
+	}
+
+
+	void LateUpdate(){
+
+		if (isFollowing)
+		{
+			transform.position = new Vector3 (player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z); 
+		}
+	}
+}
--- Steven's Test Scene/Assets/Scripts/Testing.cs
+++ Steven's Test Scene/Assets/Scripts/Testing.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Testing : MonoBehaviour {
 
 	public Transform player;
-	public Transform wayPoint1;
-	private float maxRange = 15f;
+	public int currentPoint;
+	public Transform[] wayPoints;
+	private float maxRange = 50f;
 
 	private float moveSpeed = .05f;
+	public bool following = false;
 	//private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
@@ -19,11 +21,40 @@ public class Testing : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var heading = player.position - transform.position;
+		if (transform.position == wayPoints [currentPoint].position)
+		{
+			currentPoint++;
+		}
+
+
 	
 		//transform.position = Vector3.MoveTowards (transform.position, wayPoint1.position, moveSpeed);
 		if (heading.sqrMagnitude < maxRange * maxRange)
 		{
+			following = true;
 			transform.position = Vector3.MoveTowards (transform.position, player.position, moveSpeed);
+		} else
+			following = false;
+
+
+		if (following == false)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, wayPoints [currentPoint].position, moveSpeed);
+
+			if (transform.position == wayPoints [currentPoint].position)
+			{
+				currentPoint++;
+			}
+
+			if (currentPoint >= 3)
+			{
+				currentPoint = 0;
+			}
 		}
+
+		
 	}
+
+
+
 }
