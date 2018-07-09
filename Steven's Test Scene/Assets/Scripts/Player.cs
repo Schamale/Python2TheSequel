deb9a3aa1581e5812f4076dc316d8f70b3377477
Jimmy Reilly <JimmyReilly@users.noreply.github.com>
2018-07-09 02:36:10 -0400

    Janky Rotations
    It's late, okay?

--- Steven's Test Scene/Assets/Scripts/Player.cs
+++ Steven's Test Scene/Assets/Scripts/Player.cs
@@ -10,12 +10,13 @@ public class Player : MonoBehaviour {
 	public float verticalVelocity;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		horizontalVelocity = 0f;
 		verticalVelocity = 0f;
 
@@ -23,10 +24,18 @@ public class Player : MonoBehaviour {
 		//Used to go right
 		if (Input.GetKey (KeyCode.D))
 		{
-			//GetComponent<Rigidbody2D> ().rotation = -90f;
+			//GetComponent<Rigidbody2D> ().freezeRotation;
 			//GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			horizontalVelocity = moveSpeed;
-		
+
+			transform.up = GetComponent<Rigidbody2D> ().velocity;
+
+			if (Input.GetKeyDown (KeyCode.D)) 
+			{
+				transform.Rotate (0f, 0f, -90f);
+
+				GetComponent<Rigidbody2D> ().freezeRotation = true;
+			}
 		}
 
 
@@ -37,6 +46,14 @@ public class Player : MonoBehaviour {
 			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			horizontalVelocity = -moveSpeed;
 
+			transform.up = GetComponent<Rigidbody2D> ().velocity;
+
+			if (Input.GetKeyDown (KeyCode.A)) 
+			{
+				transform.Rotate (0f, 0f, 90f);
+
+				GetComponent<Rigidbody2D> ().freezeRotation = true;
+			}
 		}
 
 
@@ -46,6 +63,14 @@ public class Player : MonoBehaviour {
 			//GetComponent<Rigidbody2D> ().rotation = 0f;
 			verticalVelocity = moveSpeed;
 
+			transform.up = GetComponent<Rigidbody2D> ().velocity;
+
+			if (Input.GetKeyDown (KeyCode.W)) 
+			{
+				transform.Rotate (0f, 0f, 0f);
+
+				GetComponent<Rigidbody2D> ().freezeRotation = true;
+			}
 		}
 
 		//Used to go down
@@ -54,15 +79,20 @@ public class Player : MonoBehaviour {
 			//GetComponent<Rigidbody2D> ().rotation = 180f;
 			verticalVelocity = -moveSpeed;
 
+			transform.up = GetComponent<Rigidbody2D> ().velocity;
+
+			if (Input.GetKeyDown (KeyCode.S)) 
+			{
+				transform.Rotate (0f, 0f, 180f);
+
+				GetComponent<Rigidbody2D> ().freezeRotation = true;
+			}
 		}
-
-
+			
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (horizontalVelocity, verticalVelocity);
 		//GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, moveVelocity);
 
-
-		transform.up = GetComponent<Rigidbody2D> ().velocity;
-
+		GetComponent<Rigidbody2D> ().freezeRotation = false;
 
 	}
 
