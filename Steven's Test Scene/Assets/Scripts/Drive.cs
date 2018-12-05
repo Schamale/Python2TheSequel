383ee424261410fe8d4be15211a655570f605a48
Schamale <38950754+Schamale@users.noreply.github.com>
2018-12-04 23:54:14 -0500

    changes

---
+++ Steven's Test Scene/Assets/Scripts/Drive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    public float speed = 5.0f;
+    void Update()
+    {
+        float translationY = Input.GetAxis("Vertical") * speed;
+        float translationX = Input.GetAxis("Horizontal") * speed;
+        translationY = Time.deltaTime;
+        translationX = Time.deltaTime;
+
+        transform.Translate(0, translationY, 0);
+        transform.Translate(translationX, 0, 0);
+    }
+}
